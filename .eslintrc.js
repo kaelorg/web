f09a8b16bd50571571a4a57b3ce2839e6c6452b6
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-10-11 17:15:36 -0300

    all fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,4 @@
 module.exports = {
   root: true,
-  extends: '@hitechline/eslint-config-node',
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        newlinesBetween: 'always',
-        alphabetize: { order: 'asc', ignoreCase: true },
-        groups: [
-          ['module', '/^@kaelbot/'],
-          ['parent', 'sibling', 'index'],
-        ],
-      },
-    ],
-  },
+  extends: '@hitechline/eslint-config-web',
 };
