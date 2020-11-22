26b1afda34471ce8e451fe4b40706e7aaebcaad0
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-22 19:42:05 -0300

    nothing

--- packages/dashboard/src/App.js
+++ packages/dashboard/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
       <PersistGate persistor={persistor}>
         <ThemeProvider theme={colors}>
           <GuildProvider history={history}>
-            <Router history={history}>
+            <Router>
               <Routes />
             </Router>
           </GuildProvider>
