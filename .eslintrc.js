7dce31dc828bd81dae7e1b7b168c39f1c9d808a3
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-22 18:25:33 -0300

    Some parse typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   extends: '@hitechline/eslint-config-web',
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
   rules: {
     camelcase: 'off',
   },
