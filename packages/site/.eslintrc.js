be828e0aa8f99eddb1457931c741454e90e06478
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-01-08 18:54:35 -0300

    fixes

--- packages/site/.eslintrc.js
+++ packages/site/.eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   extends: '@hitechline/eslint-config-web',
   rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
     'import-helpers/order-imports': [
       'warn',
       {
         newlinesBetween: 'always',
         alphabetize: { order: 'asc', ignoreCase: true },
         groups: [
-          ['module', '/^@/'],
+          ['module'],
           '/^@(api|config|styles|layout|components)/',
           ['parent', 'sibling', 'index'],
-          '/.(styl|stylus)$/',
+          '/.css$/',
         ],
       },
     ],
