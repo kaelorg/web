f265fd60430ede66353f7fd198a617038cf3c858
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-01-23 21:32:03 -0300

    chore: add `postcss-color-mod-function` plugin

--- packages/site/postcss.config.js
+++ packages/site/postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-nesting', 'postcss-flexbugs-fixes'],
+  plugins: [
+    'tailwindcss',
+    'postcss-nesting',
+    'postcss-flexbugs-fixes',
+    'postcss-color-mod-function',
+  ],
 };
